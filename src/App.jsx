8b9d9af5a5693d2be5b469b7f9d786798f0e0ad4
gchan26 <guilhermechan30@gmail.com>
2024-06-03 13:17:19 -0300

    Finished route settings

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
