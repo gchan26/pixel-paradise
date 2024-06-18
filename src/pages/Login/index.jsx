cd219aa788379ead6bb414596402d2ce583090b5
gchan26 <guilhermechan30@gmail.com>
2024-06-18 15:43:37 -0300

    Small fixes to form layout

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -45,7 +45,7 @@ const Login = () => {
                   clipRule="evenodd"
                 />
               </svg>
-              <input type="password" className="grow" value="password" />
+              <input type="password" className="grow" placeholder="Password" />
             </label>
             <a href="/forgot">
               <span className="text-sm hover:text-blue-500 hover:underline block">
