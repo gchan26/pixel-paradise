75d85bb7471abedf6db77f9c841e1b4b0690ccf6
gchan26 <guilhermechan30@gmail.com>
2024-06-18 17:34:42 -0300

    Finished  page

--- src/pages/Recover/index.jsx
+++ src/pages/Recover/index.jsx
@@ -4,39 +4,43 @@
 import { useState } from "react";
 
 const Recover = () => {
+  const [code, setCode] = useState("");
 
-    const [code, setCode] = useState('');
-
-    const handleSubmit = (event) => {
-      event.preventDefault();
-      // Handle the form submission
-      console.log('2FA Code submitted:', code);
-    }; 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("2FA Code submitted:", code);
+  };
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-[#121A23]">
-    <div className="card w-96 bg-[#1D232A] shadow-xl">
-      <div className="card-body">
-        <h2 className="card-title">Check your Email</h2>
-        <form onSubmit={handleSubmit}>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">A 6-digit recovery code was sent to your email address, enter it below:</span>
-            </label>
-            <input
-              type="text"
-              placeholder="Enter code"
-              className="input input-bordered"
-              value={code}
-              onChange={(e) => setCode(e.target.value)}
-            />
-          </div>
-          <div className="form-control mt-4">
-            <button className="btn bg-light-blue-500 hover:bg-light-blue-600" type="submit">
-              Submit
-            </button>
-          </div>
-          <span className="text-sm block mt-2">
+      <div className="card w-96 bg-[#1D232A] shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title">Check your Email</h2>
+          <form onSubmit={handleSubmit}>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">
+                  A 6-digit recovery code was sent to your email address, enter
+                  it below:
+                </span>
+              </label>
+              <input
+                type="text"
+                placeholder="Enter code"
+                className="input input-bordered"
+                value={code}
+                onChange={(e) => setCode(e.target.value)}
+              />
+            </div>
+            <div className="form-control mt-4">
+              <button
+                className="btn bg-light-blue-500 hover:bg-light-blue-600"
+                type="submit"
+              >
+                Submit
+              </button>
+            </div>
+            <span className="text-sm block mt-2">
               Don't see a code?{" "}
               <a href="#">
                 <span className="hover:text-blue-500 hover:underline font-semibold">
@@ -44,10 +48,10 @@ const Recover = () => {
                 </span>
               </a>
             </span>
-        </form>
+          </form>
+        </div>
       </div>
     </div>
-  </div>
   );
 };
 
