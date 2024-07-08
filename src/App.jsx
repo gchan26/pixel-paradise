9ecb952f588cbeb849c632d1d68137c8a6381c8a
gchan26 <guilhermechan30@gmail.com>
2024-07-08 12:55:13 -0300

    Created new file  and created page route

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Footer from "../src/components/Footer";
 
 // Pages
 import Home from "../src/pages/Home";
+import NotFound from "../src/pages/Errors/NotFound.jsx";
 import Login from "../src/pages/Login";
 import SignUp from "../src/pages/SignUp";
 import Recover from "../src/pages/Recover";
@@ -22,6 +23,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/recover" element={<Recover />} />
---
+++ src/pages/Errors/NotFound.jsx
@@ -0,0 +1,16 @@
+// React
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+// Images
+import ChestIMG from "../../assets/chest.png"
+
+const NotFound = () => {
+  return (
+		<div className="h-screen">
+            <img src={ChestIMG} /> 
+		</div>
+	)
+}
+
+export default NotFound;
