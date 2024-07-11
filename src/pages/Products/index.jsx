2b9ee87f769435d36baea3edc91bdafac6c6a6e4
gchan26 <guilhermechan30@gmail.com>
2024-07-11 15:29:33 -0300

    Created layout for products page

--- src/pages/Products/index.jsx
+++ src/pages/Products/index.jsx
@@ -1,7 +1,7 @@
-import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import products from '../../data/products';
-import ProductCard from '../../components/ProductCard';
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import products from "../../data/products";
+import ProductCard from "../../components/ProductCard";
 
 const Products = () => {
   const { filterType, filterValue } = useParams();
@@ -15,16 +15,16 @@ const Products = () => {
     return () => clearTimeout(timer);
   }, []);
 
-  const filteredProducts = products.filter(product => {
-    if (filterType === 'type') {
+  const filteredProducts = products.filter((product) => {
+    if (filterType === "type") {
       return product.type.toLowerCase() === filterValue.toLowerCase();
     }
-    
-    if (filterType === 'company') {
+
+    if (filterType === "company") {
       return product.company.toLowerCase() === filterValue.toLowerCase();
     }
 
-    if (filterType === 'category') {
+    if (filterType === "category") {
       return product.category.toLowerCase() === filterValue.toLowerCase();
     }
 
@@ -32,15 +32,43 @@ const Products = () => {
   });
 
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Products</h1>
-      <div className="relative z-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 justify-center mx-auto xl:mx-40">
-        {filteredProducts.map(product => (
-          <ProductCard key={product.id} product={product} loading={loading} />
-        ))}
-      </div>
-    </div>
+    <>
+      <section
+        id="products-section"
+        className="relative min-h-screen bg-dark-blue-700 p-10"
+      >
+        <div className="background">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <div className="relative z-10 flex justify-center w-full mb-8">
+          <h1 className="text-6xl font-retro text-dark-blue-50 text-center">
+            All Products
+          </h1>
+        </div>
+        <div className="relative z-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 justify-center mx-auto xl:mx-40">
+        {filteredProducts.map((product) => (
+              <ProductCard
+                key={product.id}
+                product={product}
+                loading={loading}
+              />
+            ))}
+
+        </div>
+      </section>
+    </>
   );
-}
+};
 
 export default Products;
