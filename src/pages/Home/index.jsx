eb102751581bebe5a95aded562039266a7dd23b7
gchan26 <guilhermechan30@gmail.com>
2024-07-11 17:39:13 -0300

    Fixed All Products route in home button

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -180,7 +180,7 @@ const Home = () => {
             <h1 className="text-2xl text-center mb-4">
               Haven't found what you're looking for?
             </h1>
-            <a href="/allproducts">
+            <a href="/products">
               <button className="btn bg-light-blue-600 hover:bg-light-blue-700 text-dark-blue-50 flex items-center justify-center w-full md:w-auto">
                 <MagnifyingGlassIcon className="size-5" />
                 See all products
