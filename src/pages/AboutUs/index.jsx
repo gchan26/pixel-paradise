6b778144357abeccdde78f61a137c66435fc9b9e
gchan26 <guilhermechan30@gmail.com>
2024-07-11 17:30:36 -0300

    Changed font and colors from the About Us page

--- src/pages/AboutUs/index.jsx
+++ src/pages/AboutUs/index.jsx
@@ -8,7 +8,7 @@ import "animate.css";
 
 const AboutUs = () => {
   return (
-    <div className="h-screen">
+    <div className="min-h-screen">
       <div className="hero bg-base-200 min-h-screen">
         <div className="hero-content flex-col lg:flex-row-reverse">
           <img
@@ -16,9 +16,9 @@ const AboutUs = () => {
             className="sm:max-w-sm max-w-72 rounded-lg shadow-2xl animate__animated animate__zoomIn"
           />
           <div className="animate__animated animate__slideInLeft">
-            <h1 className="text-6xl font-bold sm:px-0 px-4 ">About Us</h1>
-            <p className="sm:py-6 sm:px-0 p-4">
-              Our passion for preserving the golden age of video games drives us
+            <h1 className="text-6xl font-bold md:px-0 px-4 font-retro text-light-blue-400 text-center md:text-left">About Us</h1>
+            <p className="md:py-6 md:px-0 p-4">
+              Since 2003, our passion for preserving the golden age of video games drives us
               to curate a unique collection of cartridges, consoles, and
               memorabilia from the '70s, '80s, and '90s. Whether you're a
               seasoned gamer looking to relive your favorite childhood
@@ -28,7 +28,7 @@ const AboutUs = () => {
               selection of pristine, fully-functional games and systems but also
               exceptional customer service with knowledgeable staff who share
               your love for retro gaming. Step back in time with us and
-              rediscover the magic that defined an era—because some games never
+              rediscover the magic that defined an era — because some games never
               go out of style.
             </p>
           </div>
