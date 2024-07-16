f4f15a15aa78d725297046460168490ec4c56116
gchan26 <guilhermechan30@gmail.com>
2024-07-16 11:52:05 -0300

    Created AuthProvider wrapper to the routes in App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+// Env
+// import 'dotenv/config';
+
 // React Router
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -15,24 +18,28 @@ import Products from "../src/pages/Products";
 import ProductDetails from "../src/pages/ProductDetails";
 import AboutUs from "../src/pages/AboutUs";
 
+import { AuthProvider } from "./contexts/AuthContext";
+
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<NotFound />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/recover" element={<Recover />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/products/:category" element={<Products />} />
-        <Route path="/products/:company/:category" element={<Products />} />
-        <Route path="/product/:id" element={<ProductDetails />} />
-        <Route path="/about" element={<AboutUs />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/recover" element={<Recover />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:category" element={<Products />} />
+          <Route path="/products/:company/:category" element={<Products />} />
+          <Route path="/product/:id" element={<ProductDetails />} />
+          <Route path="/about" element={<AboutUs />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </AuthProvider>
   );
 }
 
