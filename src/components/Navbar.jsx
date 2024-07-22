181648691ef3b2bd59c3232b6cf6c5854242ac90
gchan26 <guilhermechan30@gmail.com>
2024-07-22 16:53:16 -0300

    Added cart and changed logout color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,6 +13,7 @@ import smallLogo from "../assets/logos/smallLogo.svg";
 const Navbar = () => {
   const { currentUser, logout } = useAuth();
   const [searchTerm, setSearchTerm] = useState("");
+  const [cartItems] = useState(8); // Example cart items number
   const navigate = useNavigate();
 
   const handleLogout = async () => {
@@ -224,7 +225,78 @@ const Navbar = () => {
             </svg>
           </label>
         </form>
-        {!currentUser ? (
+        {currentUser && (
+          <div className="flex items-center gap-4">
+            <div className="dropdown dropdown-end">
+              <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+                <div className="indicator">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 004 0z"
+                    />
+                  </svg>
+                  <span className="badge badge-sm indicator-item">{cartItems}</span>
+                </div>
+              </div>
+              <div
+                tabIndex={0}
+                className="card card-compact dropdown-content bg-base-100 z-[1] mt-3 w-52 shadow"
+              >
+                <div className="card-body">
+                  <span className="text-lg font-bold">{cartItems} Items</span>
+                  <span className="text-info">Subtotal: $999</span>
+                  <div className="card-actions">
+                    <button
+                      className="btn btn-primary btn-block"
+                      onClick={() => navigate("/my-cart")}
+                    >
+                      View cart
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="dropdown dropdown-end">
+              <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                <div className="w-10 rounded-full">
+                  {currentUser.photoURL ? (
+                    <img src={currentUser.photoURL} alt="Profile" />
+                  ) : (
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#054B9E" className="w-10">
+                      <path fillRule="evenodd" d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" clipRule="evenodd" />
+                    </svg>
+                  )}
+                </div>
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+              >
+                <h3 className="text-center flex justify-center items-center h-full font-semibold p-4">
+                  {currentUser.displayName ? (
+                    <span>{currentUser.displayName}</span>
+                  ) : (
+                    <span>{currentUser.email}</span>
+                  )}
+                </h3>
+                <li>
+                  <button className="text-red-600" onClick={handleLogout}>Logout</button>
+                </li>
+              </ul>
+            </div>
+          </div>
+        )}
+        {!currentUser && (
           <>
             <NavLink
               to="/login"
@@ -235,31 +307,6 @@ const Navbar = () => {
             <NavLink to="/login">
               <ArrowLeftEndOnRectangleIcon className="block sm:hidden size-10 p-2 text-white bg-light-blue-500 rounded-lg" />
             </NavLink>
-          </>
-        ) : (
-          <>
-          
-            <div className="dropdown dropdown-end">
-            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-              <div className="w-10 rounded-full">
-                {currentUser.photoURL ? (
-                  <img src={currentUser.photoURL} alt="Profile" />
-                ) : (
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#054B9E" className="w-10">
-                    <path fillRule="evenodd" d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" clipRule="evenodd" />
-                  </svg>
-                )}
-              </div>
-            </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-            >
-              <li>
-                <button onClick={handleLogout}>Logout</button>
-              </li>
-            </ul>
-          </div>
           </>
         )}
       </div>
