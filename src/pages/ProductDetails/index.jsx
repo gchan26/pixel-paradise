d946821eedb21b17b7d302e98b553c5ad1c7834a
gchan26 <guilhermechan30@gmail.com>
2024-07-23 11:46:51 -0300

    added handleAddToCart

--- src/pages/ProductDetails/index.jsx
+++ src/pages/ProductDetails/index.jsx
@@ -1,10 +1,15 @@
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import { useCart } from "../../contexts/CartContext";
+import { useAuth } from "../../contexts/AuthContext";
 import products from "../../data/products";
 
 const ProductDetails = () => {
   const { id } = useParams();
   const [product, setProduct] = useState(null);
+  const { addItemToCart } = useCart();
+  const { currentUser } = useAuth();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const foundProduct = products.find(
@@ -17,6 +22,14 @@ const ProductDetails = () => {
     return <div>Loading...</div>;
   }
 
+  const handleAddToCart = () => {
+    if (currentUser) {
+      addItemToCart(product);
+    } else {
+      navigate("/login");
+    }
+  };
+
   return (
     <div className="mx-auto p-4 bg-gradient-to-r from-dark-blue-800 to-dark-blue-600 min-h-screen flex items-center">
       <div className="p-4 sm:p-8 flex flex-col items-center sm:flex-row max-w-3xl mx-auto bg-blue-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100">
@@ -28,14 +41,15 @@ const ProductDetails = () => {
           />
         </div>
         <div className="p-4">
-          <h2 className="text-3xl sm:text-4xl font-semibold text-white">
-            {product.name}
-          </h2>
+          <h2 className="text-3xl sm:text-4xl font-semibold text-white">{product.name}</h2>
           <p className="text-gray-600">
             {product.category}, {product.company}
           </p>
-          <p className="text-3xl font-bold my-4">{product.price}</p>
-          <button className="btn bg-light-blue-600 hover:bg-light-blue-700 min-w-52 w-full hover:text-white">
+          <p className="text-3xl font-bold my-4">${product.price.toFixed(2)}</p>
+          <button
+            className="btn bg-light-blue-600 hover:bg-light-blue-700 min-w-52 w-full hover:text-white"
+            onClick={handleAddToCart}
+          >
             Add to Cart
           </button>
         </div>
