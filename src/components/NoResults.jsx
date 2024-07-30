8d46fd91e2d03c9010947307363236ecb553a6cb
gchan26 <guilhermechan30@gmail.com>
2024-07-30 10:02:40 -0300

    Created no results found page

--- src/components/NoResults.jsx
+++ src/components/NoResults.jsx
@@ -1,8 +1,13 @@
+import Block from "../assets/Block.gif"
+
 const NoResults = () => (
-    <div className="text-center text-white mt-8">
-      <h2 className="text-3xl font-bold text-center">No Results Found</h2>
-      <p className="mt-4 text-center">Try adjusting your search criteria or filters.</p>
-    </div>
+    <>
+        <div className="text-center text-white mt-8 flex flex-col items-center justify-center">
+            <h2 className="text-3xl font-bold text-center font-retro">No Results Found</h2>
+            <img src={Block} className="w-52"/>
+            <p className="mt-4 text-center">Try adjusting your search criteria or filters.</p>
+        </div>
+    </>
   );
   
   export default NoResults;
