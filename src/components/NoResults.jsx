cf2f53d4a3ec1d199c7f233f0283e1176fe384e7
gchan26 <guilhermechan30@gmail.com>
2024-07-30 09:32:24 -0300

    Created first draft of the noResults page

---
+++ src/components/NoResults.jsx
@@ -0,0 +1,8 @@
+const NoResults = () => (
+    <div className="text-center text-white mt-8">
+      <h2 className="text-3xl font-bold text-center">No Results Found</h2>
+      <p className="mt-4 text-center">Try adjusting your search criteria or filters.</p>
+    </div>
+  );
+  
+  export default NoResults;
