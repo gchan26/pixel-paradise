f79f2aae2efb3fa4b8e7efa4f93496504b0b1d39
gchan26 <guilhermechan30@gmail.com>
2024-08-01 10:03:36 -0300

    Created Babel Config

---
+++ babel.config.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+    presets: [
+      '@babel/preset-env',
+      '@babel/preset-react',
+      '@babel/preset-typescript',
+    ],
+  };
+  
